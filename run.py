caef5e459a7b981aba4d4149d148018b9e5a820b
alejandroZequeira <alejandrozequeirarosa@gmail.com>
2023-02-01 15:58:04 -0600

    update panel

--- run.py
+++ run.py
@@ -1,11 +1,50 @@
 from flask import *
 import plotly.express as px
+import pandas as pd
+import json
+import plotly
+import plotly.graph_objs as go
+import numpy as np
+import os
+app = Flask(__name__)
 
-app= Flask(__name__)
+
+def uploadDataSet():
+    dir = 'static/data/'
+    contenido = os.listdir(dir)
+    dataCsv = []
+    for fichero in contenido:
+        if os.path.isfile(os.path.join(dir, fichero)) and fichero.endswith('.csv'):
+            dataCsv.append(fichero)
+    return dataCsv
+
+
+def readDataSet(path):
+    return pd.read_csv(path)
+
+
+def dataSetFilter(dataPath, filter):
+    data = readDataSet(dataPath)
+
+
+def grafictCreater():
+    data = []
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+@app.route('/createTable',methods=['GET'])
+def createTable(path):
+    if not path:
+        path= request.data
+    df = pd.DataFrame(data)
+    for d in df:
+        print(d)
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',data=uploadDataSet())
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=45001, debug=True)
+    app.run(host='0.0.0.0', port=45001, debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+var graphs = {};
+var dataSet=document.querySelector('#dataSet')
+var graf = document.querySelector('#graf');
+
+
+Plotly.newPlot('graf', graphs); 
